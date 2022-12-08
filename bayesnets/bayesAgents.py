3c1c5ca47d7971ddac17029163117bb24d1ac65e
Fasero11 <gon2d3@gmail.com>
2022-12-08 13:51:01 +0100

    Fixed grading and p2

--- bayesnets/bayesAgents.py
+++ bayesnets/bayesAgents.py
@@ -152,11 +152,11 @@ def fillYCPT(bayesNet, gameState):
 
     yFactor = bn.Factor([Y_POS_VAR], [], bayesNet.variableDomainsDict())
     "*** YOUR CODE HERE ***"
-    from layout import PROB_BOTH_TOP, PROB_LEFT_TOP
+    from layout import PROB_BOTH_TOP, PROB_BOTH_BOTTOM, PROB_ONLY_LEFT_TOP, PROB_ONLY_LEFT_BOTTOM
     yFactor.setProbability({Y_POS_VAR: BOTH_TOP_VAL}, PROB_BOTH_TOP)
-    yFactor.setProbability({Y_POS_VAR: BOTH_BOTTOM_VAL}, 1 - PROB_BOTH_TOP)
-    yFactor.setProbability({Y_POS_VAR: LEFT_TOP_VAL}, PROB_LEFT_TOP)
-    yFactor.setProbability({Y_POS_VAR: LEFT_BOTTOM_VAL}, 1 - PROB_LEFT_TOP)
+    yFactor.setProbability({Y_POS_VAR: BOTH_BOTTOM_VAL}, PROB_BOTH_BOTTOM)
+    yFactor.setProbability({Y_POS_VAR: LEFT_TOP_VAL}, PROB_ONLY_LEFT_TOP)
+    yFactor.setProbability({Y_POS_VAR: LEFT_BOTTOM_VAL}, PROB_ONLY_LEFT_BOTTOM)
     "*** END YOUR CODE HERE ***"
     bayesNet.setCPT(Y_POS_VAR, yFactor)
 
